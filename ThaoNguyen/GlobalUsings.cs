ce1cdf37e7adbbd06344740f91440a24fcfbb973
thang9139 <hiimmichaelqs@gmail.com>
2025-08-10 13:35:58 +0700

    Thang thay doi

--- ThaoNguyen/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using ThaoNguyen.Data;
-global using ThaoNguyen.PageModels;
-global using ThaoNguyen.Pages;
-global using ThaoNguyen.Services;
-global using ThaoNguyen.Utilities;
---
+++ make
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Thang thay doi"
+git push origin main
+
